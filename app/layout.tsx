a0f6b509273794698b59676e33566130440874b6
Hemun Vaid <youremail@gmail.com>
2026-04-13 11:22:56 +0530

    added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import Footer from '@/components/Footer'
 import ErrorBoundary from '@/components/ErrorBoundary'
 import ScrollToTop from '@/components/ScrollToTop'
 import { AuthProvider } from '@/contexts/AuthContext'
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -49,6 +50,7 @@ export default function RootLayout({
             <Footer />
           </AuthProvider>
         </ErrorBoundary>
+        <Analytics />
       </body>
     </html>
   )
